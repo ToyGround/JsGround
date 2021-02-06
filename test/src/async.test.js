4e034b2e6015cd865d6758fcb398505e6048abb9
baekCode <baekcode@gmail.com>
2021-02-06 20:50:48 +0900

    feat : async/await jest unit test & setting

--- test/src/async.test.js
+++ test/src/async.test.js
@@ -1,4 +1,4 @@
-import {USERS, getUserById} from './async';
+import {getUserById} from './async';
 import {test} from '@jest/globals';
 
 test('[success case] getUserId => id : 1', () => {
@@ -7,4 +7,9 @@ test('[success case] getUserId => id : 1', () => {
 
 test('[fail case] getUserId => id : 1', () => {
   return getUserById(1).then(user => expect(user).toEqual({id: 2, name: '홍길동', age: 20}));
+});
+
+test('[success case] async/await id 1 => 1', async () => {
+  const result = await getUserById(1);
+  expect(result).toEqual({id: 1, name: '홍길동', age: 20});
 });

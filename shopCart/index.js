be39d485b587fe985d0774457b9d8894c86519b9
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-02 23:55:03 +0900

    feat : shopcart client api success -> itemList template rendering

--- shopCart/index.js
+++ shopCart/index.js
@@ -10,7 +10,24 @@ const createElement = object => {
 
 fetch(api).then(res => res.json()).then(res => {
   const {res_code, cartCnt, cartList} = res;
-  const cartCountElement = createElement({tagName: 'span', innerHTML: cartCnt});
+  if(res_code !== '200') return;
 
+  const cartCountElement = createElement({tagName: 'span', innerHTML: `Cart Count : ${cartCnt}`});
   rootEl.insertAdjacentElement('beforeend', cartCountElement);
+
+  if(Array.isArray(cartList)) {
+    cartList.forEach(item => {
+      const {cartInfo} = item;
+      const {itemList} = cartInfo[0];
+      const itemListWrap = itemList.reduce((acc,cur) => {
+        acc += `<li>${cur.prdNm}</li>`;
+        return acc
+      },'')
+
+      const cartListElement = createElement({tagName: 'ul', innerHTML: itemListWrap})
+      rootEl.insertAdjacentElement('beforeend', cartListElement)
+    })
+  }
+
+
 });

f879f10fd31c97ea58da5f03e58a704864d8d6fa
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-04 23:16:02 +0900

    feat : create element add event

--- shopCart/index.js
+++ shopCart/index.js
@@ -2,32 +2,40 @@ const api = 'http://localhost:8787';
 const rootEl = document.body.querySelector('#root');
 
 const createElement = object => {
-  const {tagName, innerHTML} = object;
+  const {tagName, innerHTML, className} = object;
   const element = document.createElement(tagName);
   element.innerHTML = innerHTML;
+  className && (element.className = className);
+  if(tagName === 'button') {
+    element.addEventListener('click', onClickHandler)
+  }
   return element;
 };
+
+const onClickHandler = e => {
+  console.log(e)
+}
 
 fetch(api).then(res => res.json()).then(res => {
   const {res_code, cartCnt, cartList} = res;
   if(res_code !== '200') return;
 
-  const cartCountElement = createElement({tagName: 'span', innerHTML: `Cart Count : ${cartCnt}`});
+  const cartCountElement = createElement({tagName: 'span', innerHTML: `Cart Count : ${cartCnt}`, className: 'cartCnt'});
   rootEl.insertAdjacentElement('beforeend', cartCountElement);
 
   if(Array.isArray(cartList)) {
     cartList.forEach(item => {
       const {cartInfo} = item;
       const {itemList} = cartInfo[0];
-      const itemListWrap = itemList.reduce((acc,cur) => {
-        acc += `<li>${cur.prdNm}</li>`;
-        return acc
-      },'')
 
-      const cartListElement = createElement({tagName: 'ul', innerHTML: itemListWrap})
-      rootEl.insertAdjacentElement('beforeend', cartListElement)
+      itemList.forEach(item => {
+        const buttonEl = createElement({tagName:'button', innerHTML: '버튼'});
+        const itemEl = createElement({tagName: 'div', innerHTML: item.prdNm});
+        itemEl.appendChild(buttonEl)
+
+        rootEl.insertAdjacentElement('beforeend', itemEl);
+      })
     })
   }
-
-
 });
+
